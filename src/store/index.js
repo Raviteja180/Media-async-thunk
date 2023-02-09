3e17107d5297db67eed3b1fe907fca63a804cf4a
Raviteja180 <tatipamularaviteja525@gmail.com>
2023-02-09 22:22:43 +0530

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slice/usersSlice";
+
+const store = configureStore({
+    reducer:{
+        users : usersReducer,
+    }
+})
+export {store}
+export * from './thunks/fetchUsers';
+export * from './thunks/addUser';
